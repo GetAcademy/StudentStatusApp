9d918b0f25bcea8a58524c45853468bc253eeb9d
GET Academy <terje@getacademy.no>
2024-01-05 14:47:23 +0100

    x

---
+++ StudentStatusApp/StatusChange.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentStatusApp
+{
+    internal class StatusChange
+    {
+        public string From;
+        public string To;
+
+        public StatusChange(string to, string from)
+        {
+            From = from;
+            To = to;
+        }
+    }
+}
---
+++ StudentStatusApp/StudentStatus2.cs
@@ -0,0 +1,44 @@
+﻿namespace StudentStatusApp
+{
+    internal class StudentStatus2
+    {
+        public string CurrentStatus { get; private set; }
+        private List<string> _statusHistory;
+        private List<StatusChange> _validStatusChanges;
+
+        public StudentStatus2()
+        {
+            _validStatusChanges = new List<StatusChange>
+            {
+                new ("Levert dokumentasjon", "Har søkt"),
+                new ("Fått tilbud","Levert dokumentasjon"),
+                new ("Signert kontrakt", "Fått tilbud"),
+                new ("Ikke startet", "Har søkt"),
+                new ("Ikke startet", "Signert kontrakt"),
+                new ("Ikke startet", "Levert dokumentasjon"),
+                new ("Ikke startet", "Fått tilbud"),
+                new ("Startet" , "Signert kontrakt"),
+                new ("Brutt" ,"Startet"),
+                new ("Fullført, ikke bestått" ,  "Startet"),
+                new ("Fullført og bestått", "Startet" ),
+                new ("Fullført og bestått", "Fullført, ikke bestått"),
+                new ("Ikke bestått - ikke flere forsøk igjen" , "Fullført, ikke bestått"),
+            };
+            _statusHistory = new List<string>();
+            ChangeStatus("Har søkt");
+        }
+
+        public void ChangeStatus(string newStatus)
+        {
+            var isValidChange = _validStatusChanges.Any(vsc => vsc.From == CurrentStatus && vsc.To == newStatus);
+            if (!isValidChange) return;
+            CurrentStatus = newStatus;
+            _statusHistory.Add(newStatus);
+        }
+
+        public bool HasHadStatus(string status)
+        {
+            return _statusHistory.Contains(status);
+        }
+    }
+}
